2c1b68d8d4a8663a4b9fff40e38741d324010917
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-01-16 20:18:50 +0100

    Listing posts

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -17,12 +17,12 @@ const Search = () => {
       <div>
         {loading && <p>Carregando...</p>}
         {posts && posts.length === 0 && (
-          <>
+          <div className={styles.noposts}>
             <p>Não foram encontrados posts a partir da sua busca...</p>
             <Link to="/" className="btn btn-dark">
               Voltar
             </Link>
-          </>
+          </div>
         )}
         {posts &&
           posts.map((post) => <PostDetails key={post.key} post={post} />)}
